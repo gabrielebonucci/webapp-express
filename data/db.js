a21df935a4e0767ddf70b46b149da3971180fed2
Gabriele Bonucci <g.bonucci99@gmail.com>
2025-10-30 15:20:17 +0100

    first commit

---
+++ data/db.js
@@ -0,0 +1,16 @@
+// config
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '2301',
+  database: 'movies'
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+    console.log('Connected to MySQL!', err);
+});
+
+module.exports = connection;
