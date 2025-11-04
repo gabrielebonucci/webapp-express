0ae9fecbbc01d85a74deab62a7cc8d139dc77469
Gabriele Bonucci <g.bonucci99@gmail.com>
2025-11-04 15:31:46 +0100

    CORS per le richieste frontend

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const cors = require('cors');
 // Importo la connessione 
 const connection = require('./data/db'); 
 // Importo il router per i film
 const movieRouter = require('./routers/movieRouter');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('<h1>Server Web App Movies</h1>');
